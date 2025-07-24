f1ad57678eaafae4c78a2a9d85a3cfee71d596cf
eraryios <nenya.ostapenko@gmail.com>
2025-07-24 13:45:58 +0300

    SOUUU

--- main.py
+++ main.py
@@ -10,14 +10,6 @@ SCREEN_WIDTH = 960
 SCREEN_HEIGHT = 720
 FPS = 60
 
-# Цвета
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-
 # Выбор фонов
 print('НУ ЧЁ А ФОН КАКОЙ А? ВАРИАНТЫ 1 ИЛИ 2 ТАК И ПИШИ А!')
 try:
@@ -36,46 +28,57 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("ДИАЛОГ КОМБАТ")
 clock = pygame.time.Clock()
 
-# Загрузка текстур
+# Загрузка текстур фона
 try:
     bg_back = pygame.image.load("textures/background_back.png").convert_alpha()
     bg_mid = pygame.image.load("textures/background_mid.png").convert_alpha()
     bg_front = pygame.image.load("textures/background_front.png").convert_alpha()
     bg_back_2 = pygame.image.load("textures/background_back_2.png").convert_alpha()
-    print('ТЕКСТУРЫ ЗАГРУЗИЛИСЬ УРАААААААААААААААААААААААААА')
+    print('ТЕКСТУРЫ ФОНА ЗАГРУЗИЛИСЬ УРАААААААААААААААААААААААААА')
 except Exception as e:
-    print(f"БЛИН ТЕКСТУР НЕТУ ЧЕЛ!! ТЫ НЕ ЗАГРУЗИЛ!! КАРОЧ Я ЗАГЛУШКИ СДЕЛАЮУ. ОШИБКА: {e}")
-    # Создаем заглушки для текстур
+    print(f"БЛИН ТЕКСТУР ФОНА НЕТУ ЧЕЛ!! ОШИБКА: {e}")
+    # Создаем заглушки для фона
     bg_back = pygame.Surface((SCREEN_WIDTH, 720))
-    bg_back.fill(BLUE)
+    bg_back.fill((0, 0, 100))
     bg_mid = pygame.Surface((SCREEN_WIDTH, 357))
-    bg_mid.fill(GREEN)
+    bg_mid.fill((0, 100, 0))
     bg_front = pygame.Surface((960, 329))
-    bg_front.fill(RED)
+    bg_front.fill((100, 0, 0))
     bg_back_2 = pygame.Surface((SCREEN_WIDTH, 720))
-    bg_back_2.fill((100, 100, 255))
+    bg_back_2.fill((50, 50, 150))
 
-# Масштабирование текстур
+# Масштабирование текстур фона
 bg_front = pygame.transform.scale(bg_front, (960, SCREEN_HEIGHT // 2))
 bg_mid = pygame.transform.scale(bg_mid, (SCREEN_WIDTH * 2, SCREEN_HEIGHT // 2))
 bg_back = pygame.transform.scale(bg_back, (SCREEN_WIDTH * 1.5, SCREEN_HEIGHT * 1.5))
 bg_back_2 = pygame.transform.scale(bg_back_2, (SCREEN_WIDTH * 1.5, SCREEN_HEIGHT * 1.5))
 
 # Создаем игроков
-player1 = Player(200, 360, 50, 50, RED, {
-    "up": pygame.K_w,
-    "down": pygame.K_s,
-    "left": pygame.K_a,
-    "right": pygame.K_d
-})
+player1 = Player(
+    x=200, 
+    y=720, 
+    screen=screen,
+    controls={
+        "left": pygame.K_a,
+        "right": pygame.K_d,
+        "attack": pygame.K_RCTRL
+    },
+    flip=False,
+    is_player1=True
+)
 
-player2 = Player(700, 360, 50, 50, BLUE, {
-    "up": pygame.K_UP,
-    "down": pygame.K_DOWN,
-    "left": pygame.K_LEFT,
-    "right": pygame.K_RIGHT
-})
-
+player2 = Player(
+    x=700, 
+    y=720, 
+    screen=screen,
+    controls={
+        "left": pygame.K_LEFT,
+        "right": pygame.K_RIGHT,
+        "attack": pygame.K_f
+    },
+    flip=False,
+    is_player1=False
+)
 # Параллакс-эффект
 class ParallaxBackground:
     def __init__(self):
@@ -92,35 +95,30 @@ class ParallaxBackground:
         self.max_back_offset = (bg_back.get_width() - SCREEN_WIDTH) / 2
     
     def update(self, midpoint_x):
-        # Рассчитываем прогресс (0 - левая граница, 1 - правая)
         progress = (midpoint_x - SCREEN_WIDTH // 2) / (SCREEN_WIDTH // 2)
-        progress = max(-1, min(1, progress))  # Ограничиваем от -1 до 1
+        progress = max(-1, min(1, progress))
         
-        # Обновляем позиции фона
         self.front_pos = progress * self.max_offset
         self.mid_pos = progress * self.max_mid_offset
         self.back_pos = progress * self.max_back_offset
     
     def draw(self, surface):
-        # Задний фон
         back_y = (SCREEN_HEIGHT - bg_back.get_height()) // 2
         if backgr == 1:
             surface.blit(bg_back, (self.back_pos - (bg_back.get_width() - SCREEN_WIDTH) // 2, back_y))
         else:
             surface.blit(bg_back_2, (self.back_pos - (bg_back_2.get_width() - SCREEN_WIDTH) // 2, back_y))
         
-        # Средний слой
         mid_y = 350
         surface.blit(bg_mid, (self.mid_pos - (bg_mid.get_width() - SCREEN_WIDTH) // 2, mid_y))
         
-        # Передний слой
         front_y = SCREEN_HEIGHT - bg_front.get_height()
         surface.blit(bg_front, (self.front_pos, front_y))
         surface.blit(bg_front, (self.front_pos + bg_front.get_width(), front_y))
         surface.blit(bg_front, (self.front_pos - bg_front.get_width(), front_y))
 
-# Создаем параллакс-фон
 parallax_bg = ParallaxBackground()
+
 
 # Главный игровой цикл
 running = True
@@ -129,20 +127,21 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    # Обновление игроков
     keys = pygame.key.get_pressed()
-    player1.update(keys)
-    player2.update(keys)
     
-    # Обновление фона на основе средней точки
+    # Обновление игроков
+    player1.update(keys, player2)
+    player2.update(keys, player1)
+    
+    # Обновление фона
     midpoint = get_midpoint(player1, player2)
     parallax_bg.update(midpoint[0])
     
     # Отрисовка
-    screen.fill(BLACK)
+    screen.fill((0, 0, 0))
     parallax_bg.draw(screen)
-    player1.draw(screen)
-    player2.draw(screen)
+    player1.draw()
+    player2.draw()
     
     pygame.display.flip()
     clock.tick(FPS)
--- players.py
+++ players.py
@@ -1,28 +1,169 @@
 import pygame
 
 class Player:
-    def __init__(self, x, y, width, height, color, controls):
-        self.rect = pygame.Rect(x, y, width, height)
-        self.color = color
-        self.controls = controls  # Словарь с клавишами управления
-        self.speed = 5
+    TEXTURES = [
+        'textures/base.png',
+        'textures/block.png',
+        'textures/strike1.png',
+        'textures/strike2.png',
+        'textures/strike3.png'
+    ]
     
-    def update(self, keys):
-        if keys[self.controls["up"]]:
-            self.rect.y -= self.speed
-        if keys[self.controls["down"]]:
-            self.rect.y += self.speed
-        if keys[self.controls["right"]]:
-            self.rect.x -= self.speed
-        if keys[self.controls["left"]]:
-            self.rect.x += self.speed
+    def __init__(self, x, y, screen, controls, flip=False, is_player1=True):
+        # Загрузка текстур
+        try:
+            self.original_base = pygame.image.load(self.TEXTURES[0]).convert_alpha()
+            self.original_block = pygame.image.load(self.TEXTURES[1]).convert_alpha()
+            self.original_strike1 = pygame.image.load(self.TEXTURES[2]).convert_alpha()
+            self.original_strike2 = pygame.image.load(self.TEXTURES[3]).convert_alpha()
+            self.original_strike3 = pygame.image.load(self.TEXTURES[4]).convert_alpha()
+        except:
+            print("БЛИН ТЕКСТУРЫ ИГРОКА НЕ ЗАГРУЗИЛИСЬ! ДЕЛАЮ ЗАГЛУШКИ!")
+            self.create_original_placeholder_textures()
         
-        # Ограничение движения в пределах экрана
-        self.rect.x = max(0, min(self.rect.x, 960 - self.rect.width))
-        self.rect.y = max(0, min(self.rect.y, 720 - self.rect.height))
+        # Инициализация текстур
+        self.base = self.original_base.copy()
+        self.block = self.original_block.copy()
+        self.strike1 = self.original_strike1.copy()
+        self.strike2 = self.original_strike2.copy()
+        self.strike3 = self.original_strike3.copy()
+        
+        self.current_texture = self.base
+        self.rect = self.current_texture.get_rect()
+        self.rect.midbottom = (x, y)
+        self.screen = screen
+        self.velocity = 5
+        self.controls = controls
+        self.is_attacking = False
+        self.attack_frame = 0
+        self.attack_cooldown = 0
+        self.flip = flip
+        self.should_flip = flip
+        self.is_player1 = is_player1
+        
+        # Характеристики игрока
+        self.hp = 100
+        self.max_hp = 100
+        self.BLOCK = False
+        self.last_hit_time = 0
+        self.hit_cooldown = 500  # Уменьшил кулдаун между ударами до 0.5 секунды
+        self.attack_range = 100  # Новая дальность атаки (256 / 2.5 ≈ 100)
     
-    def draw(self, surface):
-        pygame.draw.rect(surface, self.color, self.rect)
+    def create_original_placeholder_textures(self):
+        """Создает оригинальные (неотраженные) заглушки для текстур"""
+        size = (50, 80)
+        
+        self.original_base = pygame.Surface(size, pygame.SRCALPHA)
+        pygame.draw.rect(self.original_base, (255, 0, 0, 255), (0, 0, size[0], size[1]))
+        pygame.draw.polygon(self.original_base, (255, 255, 255, 255), 
+                          [(40, 10), (10, 40), (40, 70)])
+        
+        self.original_block = pygame.Surface(size, pygame.SRCALPHA)
+        self.original_block.fill((0, 255, 0, 255))
+        
+        self.original_strike1 = pygame.Surface(size, pygame.SRCALPHA)
+        self.original_strike1.fill((255, 255, 0, 255))
+        
+        self.original_strike2 = pygame.Surface(size, pygame.SRCALPHA)
+        self.original_strike2.fill((255, 165, 0, 255))
+        
+        self.original_strike3 = pygame.Surface(size, pygame.SRCALPHA)
+        self.original_strike3.fill((255, 69, 0, 255))
+    
+    def update_texture_direction(self, other_player):
+        new_flip = self.rect.centerx > other_player.rect.centerx
+        
+        if new_flip != self.should_flip:
+            self.should_flip = new_flip
+            self.base = pygame.transform.flip(self.original_base, self.should_flip, False)
+            self.block = pygame.transform.flip(self.original_block, self.should_flip, False)
+            self.strike1 = pygame.transform.flip(self.original_strike1, self.should_flip, False)
+            self.strike2 = pygame.transform.flip(self.original_strike2, self.should_flip, False)
+            self.strike3 = pygame.transform.flip(self.original_strike3, self.should_flip, False)
+            
+            if self.is_attacking:
+                if self.attack_frame < 5:
+                    self.current_texture = self.strike1
+                elif self.attack_frame < 10:
+                    self.current_texture = self.strike2
+                elif self.attack_frame < 15:
+                    self.current_texture = self.strike3
+            else:
+                self.current_texture = self.base
+    
+    def check_hit(self, other_player):
+        current_time = pygame.time.get_ticks()
+        if (self.is_attacking and 
+            self.attack_frame >= 5 and self.attack_frame < 15 and
+            abs(self.rect.centerx - other_player.rect.centerx) < self.attack_range and  # Используем новую дальность
+            abs(self.rect.centery - other_player.rect.centery) < 100 and
+            current_time - other_player.last_hit_time > self.hit_cooldown):
+            
+            damage = 1 if other_player.BLOCK else 10
+            other_player.hp = max(0, other_player.hp - damage)
+            other_player.last_hit_time = current_time
+            print(f"ИГРОК {'1' if self.is_player1 else '2'} УДАРИЛ! У ИГРОКА {'2' if self.is_player1 else '1'} ОСТАЛОСЬ {other_player.hp} HP!")
+    
+    def update(self, keys, other_player):
+        self.update_texture_direction(other_player)
+        
+        if keys[self.controls["left"]]:
+            self.rect.x += self.velocity
+        if keys[self.controls["right"]]:
+            self.rect.x -= self.velocity
+        
+        if keys[self.controls["attack"]] and self.attack_cooldown == 0:
+            self.is_attacking = True
+            self.attack_frame = 0
+            self.attack_cooldown = 30
+        
+        if self.is_attacking:
+            self.attack_frame += 1
+            if self.attack_frame < 5:
+                self.current_texture = self.strike1
+            elif self.attack_frame < 10:
+                self.current_texture = self.strike2
+            elif self.attack_frame < 15:
+                self.current_texture = self.strike3
+            else:
+                self.is_attacking = False
+                self.current_texture = self.base
+        
+        if self.attack_cooldown > 0:
+            self.attack_cooldown -= 1
+        
+        self.rect.x = max(0, min(self.rect.x, self.screen.get_width() - self.rect.width))
+        
+        # Проверка удара
+        self.check_hit(other_player)
+    
+    def draw_hp_bar(self):
+        bar_width = 200
+        bar_height = 20
+        hp_width = (self.hp / self.max_hp) * bar_width
+        
+        if self.is_player1:
+            x = 20
+            text_x = x + 10
+            color = (255, 0, 0)
+        else:
+            x = self.screen.get_width() - bar_width - 20
+            text_x = x + bar_width - 110
+            color = (0, 0, 255)
+        
+        y = 20
+        
+        pygame.draw.rect(self.screen, (50, 50, 50), (x, y, bar_width, bar_height))
+        pygame.draw.rect(self.screen, color, (x, y, hp_width, bar_height))
+        pygame.draw.rect(self.screen, (255, 255, 255), (x, y, bar_width, bar_height), 2)
+        
+        font = pygame.font.Font(None, 24)
+        text = font.render(f"Игрок {'1' if self.is_player1 else '2'}: {self.hp}/{self.max_hp}", True, (255, 255, 255))
+        self.screen.blit(text, (text_x, y - 2))
+    
+    def draw(self):
+        self.screen.blit(self.current_texture, self.rect)
+        self.draw_hp_bar()
 
 def get_midpoint(player1, player2):
     return ((player1.rect.centerx + player2.rect.centerx) // 2, 
