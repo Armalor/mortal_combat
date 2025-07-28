a99c8fd8e51bcdca40f810439b5e58065c4463cd
NNK <n.kutrukhin@digitalms.ru>
2025-07-28 23:28:22 +0300

    Добавили Боряна

--- players/player2.py
+++ players/player2.py
@@ -6,9 +6,15 @@ class Player2(Player):
     NAME = 'Человек-Борян'
 
     TEXTURES = [
-        get_pure_path('textures/2_boris_chai/base.png'),
+        get_pure_path('textures/2_boris_chai/base_resized.png'),
+        get_pure_path('textures/2_boris_chai/block_resized.png'),
+        get_pure_path('textures/2_boris_chai/strike1_resized.png'),
+        get_pure_path('textures/2_boris_chai/strike2_resized.png'),
+        get_pure_path('textures/2_boris_chai/strike3_resized.png'),
+        get_pure_path('textures/2_boris_chai/win_resized.png'),
+        get_pure_path('textures/2_boris_chai/defeat_resized.png'),
+        get_pure_path('textures/2_boris_chai/aperkot1_resized.png'),
+        get_pure_path('textures/2_boris_chai/aperkot2_resized.png'),
+        get_pure_path('textures/2_boris_chai/aperkot3_resized.png'),
         get_pure_path('textures/2_boris_chai/block.png'),
-        get_pure_path('textures/2_boris_chai/strike1.png'),
-        get_pure_path('textures/2_boris_chai/strike2.png'),
-        get_pure_path('textures/2_boris_chai/strike3.png'),
     ]
