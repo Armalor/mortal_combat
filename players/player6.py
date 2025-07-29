d23c1d495412a54b2675bda9ac6065a5db106b2c
NNK <n.kutrukhin@digitalms.ru>
2025-07-29 17:31:41 +0300

    Заготовка под Свету.

---
+++ players/player6.py
@@ -0,0 +1,19 @@
+from utils import get_pure_path
+from players import Player
+
+
+class Player5(Player):
+    NAME = "Светлан-бананан"
+
+    TEXTURES = [
+            get_pure_path('textures/6_sveta/katia_stoika-Photoroom.png'),
+            get_pure_path('textures/6_sveta/katia_block-Photoroom.png'),
+            get_pure_path('textures/6_sveta/katia_ydar1-Photoroom.png'),
+            get_pure_path('textures/6_sveta/katia_ydar2-Photoroom.png'),
+            get_pure_path('textures/6_sveta/katia_ydar3-Photoroom.png'),
+            get_pure_path('textures/6_sveta/katia_win-Photoroom.png'),
+            get_pure_path('textures/6_sveta/katia_proigrala-Photoroom.png'),
+            get_pure_path('textures/6_sveta/katia_aper1-Photoroom.png'),
+            get_pure_path('textures/6_sveta/katia_aper2-Photoroom.png'),
+            get_pure_path('textures/6_sveta/katia_aper3-Photoroom.png')
+        ]
